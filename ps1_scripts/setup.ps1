3145c670ac3f4c9ca3d5142ec065d9e3f6506e0e
Devinajar <jarrhack@proton.me>
2023-12-11 18:54:31 +0100

    Change order of the commands, added error handling

--- ps1_scripts/setup.ps1
+++ ps1_scripts/setup.ps1
@@ -1,26 +1,34 @@
 # 1. Install Powershell 7
 winget install --id Microsoft.Powershell --source winget
 
-# 2. Make a backup of the settings.json for the Win terminal
-
-## Get the path
-## Convert the settings file into an PS object
-$settingsPath = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
-$settingsObject = Get-Content $settingsPath\settings.json -raw | ConvertFrom-Json
-
-## Make a copy
-Copy-Item $settingsPath\settings.json $settingsPath\bak-settings.json
-
-# 3. Set up WSL
-# 3.1 Enable it
+# 2. Set up WSL
+# 2.1 Enable it
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 
-# 3.2 Install it
+# 2.2 Install it
 wsl --install
 
-# 3.3 Check Ubuntu installation
+# 2.3 Check Ubuntu installation
 wsl -d Ubuntu bash -c "cat /etc/os-release | grep -e '^NAME'"
 
-# 4 Change the terminal config
+# 3 Change the terminal config
 ## Source the functions
-. ".\functions.ps1"
+Import-Module -Name '.\Setup-TerminalFunctions'
+
+# 3.1 Make a backup of the settings.json for the Win terminal
+
+## Get the path
+try {
+    # Convert the settings file into an PS object
+    $settingsPath = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+    $settingsObject = ReadJsonToObject -path $settingsPath\settings.json
+
+    ## Make a copy
+    Copy-Item $settingsPath\settings.json $settingsPath\bak-settings.json
+}
+catch {
+    Write-Host "Error reading settings.json: $_"
+}
+
+
+
